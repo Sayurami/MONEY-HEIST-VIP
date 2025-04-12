71fba98d5a66980365792bd454c23a85ddb62996
Kingalfanm <153409894+Kingalfanm@users.noreply.github.com>
2025-04-12 11:55:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,10 +21,10 @@ const fsm = require('fs'); if (fsm.existsSync('config.env')) require('dotenv').c
 ╚═╝░░░░░╚═╝╚═════╝░ 𝗣𝗢𝗪𝗘𝗥𝗘𝗗 𝗕𝗬 𝗠𝗥 𝗗𝗜𝗟𝗔 𝗢𝗙𝗖
 */
 
-SESSION_ID: process.env.SESSION_ID || "", 
-MONGODB: process.env.MONGODB || "",
+SESSION_ID: process.env.SESSION_ID || "MONEY-HEIST-MD-DQ8liaZQ#VAEjwGGaP5jnWfylH10OhGeUo4wdmcUbcIABbfZaCs0-DILALK", 
+MONGODB: process.env.MONGODB || "mongodb+srv://dinethwishmitha7:J0znz84rlvWeOMdm@cluster0.1jufwgg.mongodb.net/",
 HEROKU_API: process.env.HEROKU_API || "HRKU-ed5c6fb8-14b9-4e21-b652-0df45300750f",
-OWNER_NUMBER:process.env.OWNER_NUMBER || "",
+OWNER_NUMBER:process.env.OWNER_NUMBER || "94740433291",
 
 //Github Workflows Deploy කරනව නම් පමනක් පහත Env යෙදිය යුතුය!
 GITHUB_TOKEN: process.env.GITHUB_TOKEN || "",
